f2e493ce06c23b1d9dc2d8125f9055309646c22e
Max Monciardini <maxmonciardini@icloud.com>
2022-11-11 09:45:12 -0700

    add support for sorting

--- src/types/request.ts
+++ src/types/request.ts
@@ -3,11 +3,17 @@ export type Query<T> = {
   [key in keyof T]: T[key];
 };
 
+export type Sort<T> = {
+  fieldName: keyof T;
+  sortOrder: 'ascend' | 'descend';
+};
+
 export type FindRequestParams<T> = {
   query: Query<Partial<T>>[];
   portal?: string[];
   limit?: string;
   offset?: string;
+  sort?: Sort<Partial<T>>[];
 };
 
 export type GetRecordRangeParams = {
